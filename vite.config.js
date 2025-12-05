0ab322825aa15da571c26edba3b0eebb35db61a9
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-06 05:00:49 +0700

    update vercel dan vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost/website-tempobox",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, "api")
-      }
-    }
-  }
+  build: { outDir: "dist" },
+  server: { historyApiFallback: true }
 });
+
