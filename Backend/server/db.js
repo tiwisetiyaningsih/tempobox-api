9c082904d780ad25006ade9e2e5dce152b10c6d1
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-06 18:06:49 +0700

    Refactor DB connection to use environment variables
    Updated database connection configuration to use environment variables.

--- Backend/server/db.js
+++ Backend/server/db.js
@@ -1,11 +1,11 @@
-// server/db.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'tempobox',
+    host: process.env.MYSQLHOST || 'localhost',
+    user: process.env.MYSQLUSER || 'root',
+    password: process.env.MYSQLPASSWORD || '',
+    database: process.env.MYSQLDATABASE || 'tempobox',
+    port: process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
