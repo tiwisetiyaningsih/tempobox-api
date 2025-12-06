98b3cb6b21cd8eaea3e93ebdfbd711aa48d5d383
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-07 02:58:33 +0700

    Set database port to 3306 explicitly

--- Backend/server/db.js
+++ Backend/server/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.PORT || 3306,
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
