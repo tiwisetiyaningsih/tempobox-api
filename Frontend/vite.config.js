016396d0f13aade2a57a7fb4a2dc8cffd2182555
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-06 16:26:05 +0700

    ganti struktur project

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: { outDir: "dist" },
-  server: { historyApiFallback: true }
+  build: {
+    outDir: "dist", // jangan diubah
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
-
