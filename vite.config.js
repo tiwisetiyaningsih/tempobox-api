eacef0cadf8f8994d2a4e4d5240b816d6024f8c0
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-07 16:24:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['tempobox.up.railway.app'], 
+  },
   build: { outDir: "dist" },
-  server: { historyApiFallback: true }
+  server: { 
+    historyApiFallback: true,
+    allowedHosts: ['tempobox.up.railway.app'],
+  }
 });
-
