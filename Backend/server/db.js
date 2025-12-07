48b968e1654d515b19fd5c8bbfeebcaa1ac7f764
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-07 13:54:04 +0700

    Update db.js

--- Backend/server/db.js
+++ Backend/server/db.js
@@ -5,7 +5,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
