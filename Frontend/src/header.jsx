040560d64a380b9fd513574eeb08fce4d0ea2afc
Tiwi Arum Setyaningsih <84169173+tiwisetiyaningsih@users.noreply.github.com>
2025-12-09 06:26:35 +0700

    Update header.jsx

--- Frontend/src/header.jsx
+++ Frontend/src/header.jsx
@@ -12,18 +12,17 @@ export default function Header() {
           <h1 className="fw-bold mb-4" style={{fontSize:'54px'}}>
             Temukan Gudang Aman di Bandung
           </h1>
-          <p style={{fontSize:'18px', paddingBottom:'25px'}}>
-            Cari, bandingkan, dan hubungi pemilik langsung lewat <br />WhatsApp tanpa takut penipuan.
+          <p style={{fontSize:'18px', paddingBottom:'25px', maxWidth:'500px'}}>
+            Cari, bandingkan, dan hubungi pemilik langsung lewat WhatsApp tanpa takut penipuan.
           </p>
-
+        </div>
           <a 
             href="#daftarGudang" 
             className="btn btn-outline-light fw-bold hero-btn d-flex justify-content-center align-items-center" 
-            // style={{ border: '3px solid white', borderRadius: '8px', height: '50px', width: '40%', textAlign: 'center', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow:'ellipsis'}}
           >
             Lihat Daftar Gudang Sekarang
           </a>
-        </div>
+      
       </div>
     </section>
   );
